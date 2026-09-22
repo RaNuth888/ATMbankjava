0c385b3e97a4203b9029edb4302cc26b2d5ebe6b
Tang henglay <henglaytang12@gmail.com>
2026-09-22 17:28:24 +0700

    Added checkingaccount

---
+++ CheckingAccount.java
@@ -0,0 +1,16 @@
+package ATMbankjava;
+
+public class CheckingAccount extends Account {
+
+    public CheckingAccount(
+            int accountNumber,
+            String name,
+            String pin,
+            double balance) {
+        super(accountNumber, name, pin, balance);
+    }
+    @Override
+    public void showAccountType() {
+        System.out.println("Account Type: Checking");
+    }
+}
